2dd588a37115961db2e21e8edad2774b1f1fa69c
Chad Nickell <chad@yaguara.co>
2019-05-25 15:02:43 -0600

    Adjust desktop widgets in i3

--- arch/install.sh
+++ arch/install.sh
@@ -7,6 +7,7 @@ info 'Installing'
 overwrite_all=false backup_all=false skip_all=false
 mkdir -p $HOME/.bin
 chmod u+x $HOME/.dotfiles/arch/change-wallpaper.sh
+chmod u+x $HOME/.dotfiles/arch/conky-desktop.sh
 link_file $HOME/.dotfiles/arch/change-wallpaper.sh $HOME/.bin/change-wallpaper
 link_file $HOME/.dotfiles/arch/i3config $HOME/.i3/config
 mkdir -p $HOME/.config/i3status
