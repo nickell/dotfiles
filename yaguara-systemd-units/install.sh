a0e973b6157c565eb1d54e32b7abcd843369ca15
Chad Nickell <chad@yaguara.co>
2020-04-05 17:50:08 -0600

    Use linkf for systemd units

--- yaguara-systemd-units/install.sh
+++ yaguara-systemd-units/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+source "$HOME/.dotfiles/generic/shell/functions/colored_echos.sh"
+source "$HOME/.dotfiles/generic/shell/functions/linkf.sh"
+
 parentServices=(app backend)
 individualServices=(api api-watch websockets cronjobs etl mailer front-end)
 
@@ -7,7 +10,7 @@ allServices=("${parentServices[@]}")
 allServices+=("${individualServices[@]}")
 
 link() {
-    ln -sf $HOME/.dotfiles/yaguara-systemd-units/"$1".service $HOME/.config/systemd/user/"$1".service
+    linkf $HOME/.dotfiles/yaguara-systemd-units/"$1".service $HOME/.config/systemd/user/"$1".service
 }
 
 for service in "${allServices[@]}"
